ef25893432fc87d74a2dba78d8ef6fe16f6fe96c
Leo Terziman <leo.terziman@clarte.asso.fr>
2019-10-11 16:12:06 +0200

    Added example scene for net tegotiation protocol

---
+++ Assets/Examples/Net/Negotiation/Logs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net;
+using UnityEngine;
+
+namespace CLARTE.Net.Negotiation
+{
+	public class Logs : MonoBehaviour
+	{
+		public String role = "client";
+
+		public void OnConnected(IPAddress address, Guid guid, ushort channel)
+		{
+			Debug.LogFormat("Connected as {0} on IP '{1}', connection '{2}' on channel '{3}'.", role, address, guid, channel);
+		}
+
+		public void OnDisconnected(IPAddress address, Guid guid, ushort channel)
+		{
+			Debug.LogFormat("Disconnected as {0} on IP '{1}', connection '{2}' on channel '{3}'.", role, address, guid, channel);
+		}
+
+		public void OnException(IPAddress address, Guid guid, ushort channel, Exception exception)
+		{
+			Debug.LogFormat("Exception thrown for {0} on IP '{1}', connection '{2}' on channel '{3}': {4} - {5}\n{6}.", role, address, guid, channel, exception.GetType(), exception.Message, exception.StackTrace);
+		}
+
+		public void OnReceived(IPAddress address, Guid guid, ushort channel, byte[] data)
+		{
+			Debug.LogFormat("Received data as {0} on IP '{1}', connection '{2}' on channel '{3}': {4} bytes.", role, address, guid, channel, data.Length);
+		}
+
+		public void OnProgress(IPAddress address, Guid guid, ushort channel, float progress)
+		{
+			Debug.LogFormat("Received progress as {0} on IP '{1}', connection '{2}' on channel '{3}': {4} %.", role, address, guid, channel, progress * 100);
+		}
+	}
+}
---
+++ Assets/Examples/Net/Negotiation/Ping.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Net;
+using UnityEngine;
+
+namespace CLARTE.Net.Negotiation
+{
+	[RequireComponent(typeof(Base))]
+	public class Ping : MonoBehaviour
+	{
+		protected Base network;
+
+		void Start()
+		{
+			network = GetComponent<Base>();
+
+			if(network is Client)
+			{
+				((Client) network).Connect();
+			}
+		}
+
+		public void OnConnected(IPAddress address, Guid guid, ushort port)
+		{
+			if(network is Client)
+			{
+				network.SendAll(0, new byte[1]);
+			}
+		}
+
+		public void OnReceive(IPAddress address, Guid guid, ushort port, byte[] data)
+		{
+			StartCoroutine(Reply(data));
+		}
+
+		protected IEnumerator Reply(byte[] data)
+		{
+			yield return new WaitForSeconds(1f);
+
+			network.SendAll(0, data);
+		}
+	}
+}
